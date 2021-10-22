26d659dddd5fcda0c0de5f4571c83983ab92d444
shinadori <work.zzp1993@gmail.com>
2021-10-22 15:56:18 +0800

    correct a small bug

--- src/func/arrange.py
+++ src/func/arrange.py
@@ -47,9 +47,9 @@ def udf_arrange(names, nums_to_arrange):
 @xw.func
 @xw.arg('names', np.array, ndim=1, numbers=str, empty='', doc='Select the range of names you want to arrange for')
 @xw.arg('tasks', np.array, ndim=1, numbers=str, empty='', doc='Select the range of tasks you want to arrange for')
-@xw.arg('nums_to_arrange', np.array, numbers=str, ndim=2, empty=0, transpose=True, doc='Select the range of arrange nums')
+@xw.arg('nums_to_arrange', np.array, ndim=2, empty=0, transpose=True, doc='Select the range of arrange nums')
 @xw.ret(expand='table', transpose=True)
-def udf_arrange_mutli(names, tasks, nums_to_arrange):
+def udf_arrange_multi(names, tasks, nums_to_arrange):
     if names.shape[0] * tasks.shape[0] != nums_to_arrange.shape[0] * nums_to_arrange.shape[1]:
         return 'Incorrect input. The product of the num of names and tasks is diff from the num of nums_to_arrange'
     else:
@@ -58,6 +58,8 @@ def udf_arrange_mutli(names, tasks, nums_to_arrange):
                 res = [[], []]
                 for no_task, task in enumerate(tasks):
                     for no_name, name in enumerate(names):
+                        log.info(nums_to_arrange[no_task][no_name])
+                        log.info(type(nums_to_arrange[no_task][no_name]))
                         res[0].extend([task] * int(float(nums_to_arrange[no_task][no_name])))
                         res[1].extend([name] * int(float(nums_to_arrange[no_task][no_name])))
                 return res
